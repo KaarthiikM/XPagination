0533c668c7f6bb4fe384dc4265bf7b19b8bced83
KAARTHIIK M <kaarthiikmr001@gmail.com>
2024-03-03 21:46:42 +0530

    pagination 10 per page

--- xpagination/src/Components/EmployeeData.jsx
+++ xpagination/src/Components/EmployeeData.jsx
@@ -15,7 +15,7 @@ const EmployeeData = () => {
                 setTotalPages(Math.ceil(totalEmployees / 10));
             }
             catch(err){
-                alert("Failed to fatch data",err)
+                alert("failed to fatch data")
             }
         }
         fetchData()
